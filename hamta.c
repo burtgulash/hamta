a2b763285632da6304bb2321acfaebb7bc6074d4
burtgulash <burtgulash@gmail.com>
2016-01-11 17:35:47 +0100

    more

--- hamta.c
+++ hamta.c
@@ -1,7 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdbool.h>
 #include "hamta.h"
+
+#define CHUNK_SIZE 5
+
+bool thing_equals(thing_t *a, thing_t *b) {
+    if (a->len != b->len)
+        return false;
+    for (size_t i = 0; i < a->len; i++)
+        if (((char*) a)[i] != ((char*) b)[i])
+            return false;
+    return true;
+}
+
+hamt_node_t* new_hamt_node() {
+    hamt_node_t *node = (hamt_node_t*) malloc(sizeof(hamt_node_t));
+    return node;
+}
+
+thing_t* hamt_node_search(hamt_node_t *node, uint32_t hash, int lvl,
+                                                            thing_t *key) {
+    assert(node != NULL);
+
+    int hash_chunk = hash;
+    hash_chunk <<= (32 - lvl * CHUNK_SIZE);
+    hash_chunk >>= (lvl * CHUNK_SIZE + CHUNK_SIZE);
+
+    int shifted = node->bitmap >> hash_chunk;
+    bool child_exists = shifted & 1;
+    if (child_exists) {
+        int child_position = __builtin_popcount(shifted >> 1);
+        hamt_node_t *subnode = (node->children)[child_position];
+        hamt_node_t **children = subnode->children;
+        int children_ptr_val = *((int*) &children);
+        if (children_ptr_val & 1 == KEY_VALUE_T_FLAG) {
+            // clear flag bit before dereferencing
+            children_ptr_val &= ~1;
+            // convert back to original type without the flag bit
+            children = *((hamt_node_t***) &children_ptr_val);
+        } else {
+            key_value_t *leaf = (key_value_t*) subnode;
+            if (thing_equals(leaf->key, key))
+                return leaf->value;
+            else {
+            }
+        }
+
+
+    } else
+        return NULL;
+
+    return NULL;
+}
 
 hamt_t* new_hamt() {
     hamt_t *h = (hamt_t*) malloc(sizeof(hamt_t));
@@ -19,9 +71,13 @@ uint32_t fnv1(void *key, size_t len) {
     return hash;
 }
 
-void insert(hamt_t *trie, void *key, size_t key_len,
-            void *value, size_t value_len) {
-    uint32_t hash = fnv1(key, key_len);
+void hamt_insert(hamt_t *trie, thing_t *key, thing_t *value) {
+    uint32_t hash = fnv1(key->x, key->len);
+}
+
+void* hamt_find(hamt_t *trie, thing_t *key) {
+    uint32_t hash = fnv1(key->x, key->len);
+    return hamt_node_search(trie->root, hash, 0, key);
 }
 
 void main() {
--- hamta.h
+++ hamta.h
@@ -2,9 +2,20 @@
 #define HAMTA_H
 #include <stdint.h>
 
+// do not change macro of these values!  objects's last bit will be set to 1 if
+// it has type hamt_node_t and 0 if key_value_t. This is possible since malloc
+// will align to some multiple of even number.
+#define KEY_VALUE_T_FLAG 0
+#define HAMT_NODE_T_FLAG 1
+
+typedef struct thing {
+    void *x;
+    size_t len;
+} thing_t;
+
 typedef struct key_value {
-    void *key;
-    void *value;
+    thing_t *key;
+    thing_t *value;
 } key_value_t;
 
 typedef struct hamt_node {
